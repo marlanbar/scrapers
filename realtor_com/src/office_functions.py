1eb11abf12255b45cfae3ea4b3229881672ed054
Martin Ezequiel Langberg <martinlangberg@gmail.com>
2018-08-21 16:37:37 -0300

    office functions

--- realtor_com/src/office_functions.py
+++ realtor_com/src/office_functions.py
@@ -59,7 +59,7 @@ def get_html(url, proxies, timeout=5, tries=5):
 def get_pages(term, proxies):
     pages = []
     page_number = 1
-    url_base = "https://www.realtor.com/realestateagents/"
+    url_base = "https://www.realtor.com/realestateagency/"
     while True:
         try:
             html = get_html(url_base + term + "/pg-{}".format(page_number), proxies, tries=10)
@@ -91,73 +91,33 @@ def has_next_button(soup):
     else:
         return button.find("a")
 
-def get_agents_urls(list_obj):
-    prefix = "https://www.realtor.com/"
-    urls = []
+# Split the raw page source into segments, one for each home listing.
+def get_offices(list_obj):
+    output = []
     for soup in list_obj:
-        urls += [prefix + item["data-url"] for item in soup.find_all('div', {'class' : 'agent-list-card clearfix'})]
-    return urls
+        htmlSplit = soup.find_all('div', {'class' : "agent-list-card clearfix"})
+        output += htmlSplit
+    return(output)
 
-def get_agent_name(soup):
+def get_office_name(soup):
     try:
-        name = soup.find("span", {"class":"agent-name"}).get_text().split(",")[0].strip()
+        name = soup.find("div", {"class":"agent-name text-bold"}).get_text()
     except (ValueError, AttributeError, TypeError):
         name = "NA"
     return name.replace("\"", "").strip()
 
 def get_phone(soup):
     try:
-        phone = soup.find("span", {"itemprop":"telephone"}).get_text()
-    except (ValueError, AttributeError):
-        phone = "NA"
-    return phone.replace("\"", "").strip()
-
-def get_agency(soup):
-    try:
-        agency = soup.find("span", {"class": "brokerage-title"})["content"]
+        name = soup.find("div", {"itemprop":"telephone"}).get_text()
     except (ValueError, AttributeError, TypeError):
-        agency = "NA"
-    return agency.replace("\"", "").strip()
+        name = "NA"
+    return name.replace("\"", "").strip()
 
-def get_houses(soup):
-    houses = []
-    house_soups = soup.find_all(attrs={"data-prop-full-address":True})
-    for house_soup in house_soups:
-        if house_soup:
-            house = []
-            house.append(house_soup["data-prop-full-address"])
-            house.append(house_soup["data-prop-lat"])
-            house.append(house_soup["data-prop-long"])
-            house.append(house_soup["data-prop-date"])
-            house.append(house_soup["data-prop-stats-text"])
-            house.append(house_soup["data-prop-bed-bath"])
-            house.append(house_soup["data-prop-price"].replace(",", "").replace("$", ""))
-            # If the house is still on sale this field does not exist.
-            try:
-                house.append(house_soup["data-prop-presented"].split("Worked with ")[1])
-            except:
-                house.append("NA")
-            houses.append(house)
-        else:
-            continue
-    return houses
-
-def get_new_obs(url, proxies):
+def get_new_obs(soup):
     new_obs = []
-    try:
-        html = get_html(url, proxies)
-    except RequestException:
-        return new_obs
-    soup = BeautifulSoup(html, "lxml")
-    agent_name = get_agent_name(soup)
-    phone = get_phone(soup)
-    houses = get_houses(soup)
-    agency = get_agency(soup)
-    for house in houses:
-        new_obs.append([agent_name, phone, agency] + house)
+    new_obs.append(get_office_name(soup))
+    new_obs.append(get_phone(soup))
     return new_obs
 
-def flatten(list_obj):
-    f = lambda z: [x for y in z for x in y]
-    return f(list_obj)
+
 
